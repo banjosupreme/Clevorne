b5a1fceb697ddfa937977fb31b0b9e1039dbc580
unknown <breadandcheese@yahoo.com>
2015-04-08 23:57:05 -0400

    Basic grid lines and choice of axis variables

--- Clevorne.js
+++ Clevorne.js
@@ -43,10 +43,10 @@ var clevorne = function(pane, data){
     
 
     this.mapX = function(value){
-        return 0.05*this.paneWidth + (value - this.xmin)*0.9*this.paneWidth/this.xrange;    
+        return 0.06*this.paneWidth + (value - this.xmin)*0.88*this.paneWidth/this.xrange;    
     }
     this.mapY = function(value){ 
-        return 0.05*this.paneHeight + (this.ymax - value)*0.9*this.paneHeight/this.yrange;
+        return 0.06*this.paneHeight + (this.ymax - value)*0.88*this.paneHeight/this.yrange;
     }
     
 
@@ -73,34 +73,56 @@ var clevorne = function(pane, data){
         pane.appendChild(line);
     }
 
-    this.drawScatter = function(){
+    this.drawScatter = function(xcol, ycol){
         var x1, y1, colour;
         
         for(var j = 0; j < dataset.length; ++j){
             colour =  palette[j%palette.length];
         
             for(var k = 0; k < dataset[j].data.length; ++k){ 
-                x1 = this.mapX(dataset[j].data[k][0]);
-                y1 = this.mapY(dataset[j].data[k][1]);   
+                x1 = this.mapX(dataset[j].data[k][xcol]);
+                y1 = this.mapY(dataset[j].data[k][ycol]);   
                 this.drawPoint(x1, y1, colour);
             }
         }
     }
     
-    this.drawLines = function(){
+    this.drawLines = function(xcol, ycol){
         var x1, x2, y1, y2, colour;
         
         for(var j = 0; j < dataset.length; ++j){
             colour =  palette[j%palette.length];
-            x1 = this.mapX(dataset[j].data[0][0]);
-            y1 = this.mapY(dataset[j].data[0][1]);                
+            x1 = this.mapX(dataset[j].data[0][xcol]);
+            y1 = this.mapY(dataset[j].data[0][ycol]);                
             for(var k = 1; k < dataset[j].data.length; ++k){ 
-                x2 = this.mapX(dataset[j].data[k][0]);
-                y2 = this.mapY(dataset[j].data[k][1]);
+                x2 = this.mapX(dataset[j].data[k][xcol]);
+                y2 = this.mapY(dataset[j].data[k][ycol]);
                 this.drawLine(x1, y1, x2, y2, colour); 
                 x1 = x2;
                 y1 = y2;
             }
+        }
+    }
+    
+    this.placeGrid = function(xgrid, ygrid){
+        var gridMinX, gridMinY, gridMaxX, gridMaxY, x1, x2, y1, y2;
+        //really should make these 0.01 and 0.99 functions of some padding variable
+        gridLowX = this.paneWidth*0.01
+        gridHighX = this.paneWidth*0.99
+        
+        
+        gridLowY = this.paneHeight*0.99;
+        gridHighY = this.paneHeight*0.01;
+        
+        for(var j = 0; j < xgrid.length; ++j)
+        {
+            x1 = this.mapX(xgrid[j]);
+            this.drawLine(x1, gridLowY, x1, gridHighY, "lightgray");
+        }
+        for(var j = 0; j < ygrid.length; ++j)
+        {
+            y1 = this.mapY(ygrid[j]);
+            this.drawLine(gridLowX, y1, gridHighX, y1, "lightgray");
         }
     }
     
